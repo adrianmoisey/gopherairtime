fdde15209e5a307745811b8cdb3cd131a7ad7b90
Adrian Moisey <adrian@changeover.za.net>
2014-07-21 20:40:14 +0200

    Remove database settings completely

--- gopherairtime/settings.py
+++ gopherairtime/settings.py
@@ -15,12 +15,7 @@ def abspath(*args):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+#DATABASE
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'please-change-me'
