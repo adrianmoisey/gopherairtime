4ba723fe2a8187debc021c4f33bcd3da925fe071
Adrian Moisey <adrian@changeover.za.net>
2014-07-21 15:52:34 +0200

    Revert "Remove imported settings, configs will do that"
    This reverts commit ec8b173e34d209eb9e404940e876b4c1ccd623db.

--- gopherairtime/settings.py
+++ gopherairtime/settings.py
@@ -263,3 +263,10 @@ KATO_KEY = ""
 # ======================================================
 SMS_CONFIG = {"sender_type": "logging"}
 VUMIGO_API_URL = ""
+
+from api_settings import *
+
+try:
+    from production_settings import *
+except ImportError:
+    pass
